297781d29f51aa719463de16583dcf2dcf934e23
bhaasobh <31213499+bhaasobh@users.noreply.github.com>
2025-01-08 20:59:20 +0200

    fix user

--- models/usersModels.js
+++ models/usersModels.js
@@ -1,7 +1,7 @@
 const {mongoose, Collection}= require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name : {type : String , required : true},
+    username : {type : String , required : true},
     password :{type : String , required : true}
 },
 {collection :'users'});
