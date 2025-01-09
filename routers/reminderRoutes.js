6140748930ea202068fdbcdbe1d490a553e09541
bhaasobh <31213499+bhaasobh@users.noreply.github.com>
2025-01-09 16:36:41 +0200

    finish reminders

--- routers/reminderRoutes.js
+++ routers/reminderRoutes.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 const reminderController = require('../controllers/reminderController');
 
-// Define routes
-router.post('/addreminder', reminderController.addReminder);
+// Get all reminders
+router.get('/', reminderController.getReminders);
+
+// Get a specific reminder by ID
+router.get('/:id', reminderController.getReminderById);
+
+// Add a reminder
+router.post('/add', reminderController.addReminder);
+
+// Update a reminder
+router.put('/:id', reminderController.updateReminder);
+
+// Delete a reminder
+router.delete('/:id', reminderController.deleteReminder);
 
 module.exports = router;
