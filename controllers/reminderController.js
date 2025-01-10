012593bb8bbd220201d3a7a6c914d5e475fa1e84
bhaasobh <31213499+bhaasobh@users.noreply.github.com>
2025-01-10 09:16:48 +0200

    remindercontroller

--- controllers/reminderController.js
+++ controllers/reminderController.js
@@ -1,29 +1,6 @@
 const Reminder = require('../models/reminderModel');
 const bcrypt = require('bcrypt');
 
-exports.addReminder = async (req, res) => {
-    try {
-        const { reminderType ,title,address,Details} = req.body;
-
-        if ( !reminderType || !title || !address || !Details) {
-            return res.status(400).json({ error: "Missing required fields, including password." });
-        }
-
-            const newReminder = new Reminder({
-                reminderType ,title,address,Details
-            });
-            await newReminder.save();
-            return res.status(201).json({ message: "Reminder added successfully", Reminder: newReminder });
-        
-    } catch (err) {
-        console.error("Error adding reminder :", err.message);
-        res.status(500).json({ error: "Internal server error => ",
-                                errormassae : err.message,
-                                err : err
-        });
-    }
-};
-
 
 
 exports.addReminder = async (req, res) => {
